aea82449a278084f94d7d4a522f012e7e36cd3a9
Nikhil Pachpande <pachpanden2@gmail.com>
2022-12-20 09:00:06 -0500

    added new components

--- portfolio-ui/src/app/app.module.ts
+++ portfolio-ui/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExpComponent } from './work-exp/work-exp.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EducationComponent,
+    WorkExpComponent,
+    SkillsComponent,
+    PersonalInfoComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
