a5eef428500cfeec145ab200820c04b072e60bac
Nikhil Pachpande <pachpanden2@gmail.com>
2022-12-20 11:11:24 -0500

    added directive to make the website responsive

--- portfolio-ui/src/app/home/home.component.ts
+++ portfolio-ui/src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  
+  constructor(private breakpointObserver: BreakpointObserver){}
+  
+  ngOnInit(): void {
+    
+  }
 }
